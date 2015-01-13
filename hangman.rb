a443990c68fc546c06e78113bd249afe881023dc
Evan Koch <evankoch@gmail.com>
2015-01-12 21:03:33 -0500

    rubocop on hangman, plus a few tweaks based on class lecture

--- hangman.rb
+++ hangman.rb
@@ -1,20 +1,10 @@
 ## CLASSIC HANGMAN
 require 'set'
 
-words = ['angus',
-         'beefsteak',
-         'tomato',
-         'chocolate',
-         'gangster',
-         'unfortunate',
-         'Fargo',
-         'ASCII',
-         'drippings',
-         'dictator',
-         'capitalist',
-         'opportunist']
+words = %w(angus beefsteak tomato chocolate gangster unfortunate
+           Fargo ASCII drippings dictator capitalist opportunist)
 
-def finished? (turns, guesses, answer)
+def finished?(turns, guesses, answer)
   turns.zero? || guessed_it?(answer, guesses)
 end
 
@@ -26,11 +16,23 @@ end
 def greeting(turns)
   puts "Guess my word, and you won't hang the HANGMAN!"
   puts "You have #{turns} turns before he hangs!"
-  # TODO: tell the player about hangman, incl guess the word in x turns
+end
+
+def prompt_player
+  newguess = $stdin.gets.chomp.upcase
+  until newguess =~ /[A-Za-z]/
+    print 'You may only guess a Single Letter! Try again ==>  '
+    newguess = $stdin.gets.chomp.upcase
+  end
+  newguess
+end
+
+def show_turns(turn_count)
+  puts "You now have #{turn_count} guesses left! "
 end
 
 def game_status(answer, guesses)
-  letters = answer.split("")
+  letters = answer.split('')
   letters.each do |c|
     if guesses.include?(c) # has been guessed
       print c
@@ -39,55 +41,38 @@ def game_status(answer, guesses)
     end
 
   end
-  print "     "
+  print '       '
 end
 
 def game_over(reveal_word)
-  puts "Oh No!  You have hanged the HANGMAN!"
+  puts 'Oh No!  You have hanged the HANGMAN!'
   puts "My Word was: #{reveal_word}"
-  # TODO: tell the player what the word was and if they won or lost
-end
-
-def prompt_player
-  print "Guess a letter in my Word ==> "
-  newguess = $stdin.gets.downcase.chomp
-  return newguess
-  # TODO: get user input / show some status
 end
 
 def hangman(wordlist)
-  turn_count = ARGV.empty? ? 6 : ARGV[0].to_i
-  # if ARGV is empty, then default turn_count is 6.  If ARGV is passed
-  # then that number is passed to turn_count.
-  
-  guessed = Set.new
-  # creates a new Set called guessed {}
-  
-  answer = wordlist.sample(1)[0].downcase
-  # answer is string, randomly pulled from words array
-  # the zero references the first element in the random sample array
-  
-  greeting(turn_count) # TODO: Do I need an argument? maybe turncount
-  
+  turn_count = ARGV.empty? ? 6 : ARGV[0].to_i # ARGV passes game turn_count is 6.
+  # if no ARGV, default turn_count is 6
+  guessed = Set.new # creates a new Set called guessed {}
+  answer = wordlist.sample.upcase # answer is random sample string from [wordlist]
+  greeting(turn_count)
+
   until finished?(turn_count, guessed, answer) || guessed_it?(answer, guessed)
-
     game_status(answer, guessed)
-
     guess = prompt_player
-
     guessed.add(guess)
     unless answer.include?(guess)
       turn_count -= 1
-      puts "#{guess.upcase} is not in My Word"
+      puts "#{guess} is not in My Word"
+      show_turns(turn_count)
     end
   end
-  
+
   if guessed_it?(answer, guessed)
     puts answer
-    puts " You win!! My Word was: #{answer.upcase}"
-    puts "You have saved the HANGMAN!"
+    puts "  You win!! My Word was: #{answer.upcase}"
+    puts '  You have saved the HANGMAN!'
   else
-    game_over(answer) # TODO: Do I need an argument?
+    game_over(answer)
   end
 end
 
