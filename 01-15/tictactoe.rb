8fe846b578bfe13d83e3e739113837a646c16818
Evan Koch <evankoch@gmail.com>
2015-01-20 00:42:06 -0500

    intended version of TTT

--- 01-15/tictactoe.rb
+++ 01-15/tictactoe.rb
@@ -1,24 +1,7 @@
 ## CLASSIC Tic-Tac-Toe
 #require 'pry'
+require './tictactoe_board.rb'
 require 'set'
-
-
-class Board
-
-  attr_accessor :board, :open_spot, :turn_count
-
-  def initialize()
-    @board = new_board
-    @open_spot = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
-    @turn_count = 9
-  end
-
-  def new_board 
-    return {"a"=>" ", "b"=> " ", "c"=> ".", "d"=>" ", "e"=>"_", "f"=> " ",
-            "g"=>".","h"=>" ","i"=>" "
-    }
-  end
-end
 
 class Tictactoe
 
